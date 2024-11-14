6ce8ca8b13d644fafbb985e8e251cad2849191e0
Sarah Ng <93563644+sarah-ernee@users.noreply.github.com>
2024-11-14 22:17:19 +0800

    implement pagination and added styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
 
 const App = () => {
   const [repos, setRepos] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
 
+  // Trigger the API when page number changes
   useEffect(() => {
     const fetchRepos = async () => {
       setLoading(true);
@@ -15,32 +17,44 @@ const App = () => {
 
       const dateString = date.toISOString().split("T")[0];
 
-      const response = await fetch(
-        `https://api.github.com/search/repositories?q=created:>${dateString}&sort=stars&order=desc&page=${page}`
-      );
+      try {
+        const response = await fetch(
+          `https://api.github.com/search/repositories?q=created:>${dateString}&sort=stars&order=desc&page=${page}`
+        );
 
-      const data = await response.json();
-      
-      setRepos((prevRepos) => [...prevRepos, ...data.items]);
-      setLoading(false);
+        const data = await response.json();
+
+        if (data.items.length === 0) {
+          setHasMore(false); // no more items to display
+        }
+
+        setRepos((prevRepos) => [...prevRepos, ...data.items]);
+      } catch (error) {
+        console.error("Error fetching repos:", error);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchRepos();
   }, [page]);
 
-  const handleScroll = () => {
+  // Page number change with scroll event
+  const handleScroll = useCallback(() => {
     if (
       window.innerHeight + document.documentElement.scrollTop >=
-      document.documentElement.offsetHeight
+        document.documentElement.offsetHeight - 200 &&
+      !loading &&
+      hasMore
     ) {
       setPage((prevPage) => prevPage + 1);
     }
-  };
+  }, [loading, hasMore]);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  }, [loading, hasMore, handleScroll]);
 
   return (
     <div className="app">
@@ -53,9 +67,11 @@ const App = () => {
               alt={`${repo.owner.login} avatar`}
               className="avatar"
             />
+
             <div className="repo-details">
               <h2>{repo.name}</h2>
               <p>{repo.description || "No description available"}</p>
+
               <p>
                 ⭐ {repo.stargazers_count} | By {repo.owner.login}
               </p>
@@ -63,7 +79,9 @@ const App = () => {
           </div>
         ))}
       </div>
+
       {loading && <p className="loader">Loading...</p>}
+      {!hasMore && <p className="end-message">No more repos available</p>}
     </div>
   );
 };
